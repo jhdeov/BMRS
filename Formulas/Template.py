951bc348224ac65779a4eece1beae6cc52b5e0c4
Hossep <hovdeov@gmail.com>
2020-09-27 17:58:26 -0700

    Changed get functions into a generic templates in the Template file

--- Formulas/Template.py
+++ Formulas/Template.py
@@ -4,9 +4,9 @@ Replace TO DO with your own code
 
 def personal_setup(self):
     """Make sure that you include the symbols # and % in your list of labels"""
-    self.labels_list = ['#', '%', TO DO ]
+    self.labels_list = ['#', '%',  "REPLACE ME" ]
     """List the copies in your copy set, e.g., [1] or [1,2] or [1,2,3] or etc."""
-    self.copyset = TO DO
+    self.copyset = "REPLACE ME"
     #Example:
     #self.copyset = [1]
     """Wil you write your input strings using the above labels? if yes, then assign the following boolean to true 
@@ -14,7 +14,7 @@ def personal_setup(self):
     Generally, assign the boolean to true if you will use generic symbols like {a,b,c,...}, but set it to false if 
     you will use phonological faetures but represent your input strings with letters"""
 
-    self.labels_are_input= TO DO
+    self.labels_are_input=  "REPLACE ME"
     #Example:
     #self.labels_are_input= False
 
@@ -27,7 +27,7 @@ def personal_features(self):
     self.segment_to_labels = {
         '#': frozenset(['#']),
         '%': frozenset(['%']),
-        TO DO
+        "REPLACE ME": frozent([ "REPLACE ME"]),
     #Example:
     #    'p': frozenset(['labial', 'consonant']),
      #   'L\'': frozenset(['L', 'stress']),
@@ -37,7 +37,7 @@ def personal_features(self):
 
 def personal_predicate_setup(self):
     """Give a list of your predicates as string"""
-    self.predicates_list = TO DO
+    self.predicates_list =  "REPLACE ME"
     #Example:
     # self.predicates_list = ['initial','final','syll','clash','lapse','only']
 
@@ -50,38 +50,47 @@ def personal_Output_Formula(self, copy, label, domain_element):
 
     Make sure to use return statements
 
-    When referring to an input label, use the formula "self.input_to_labels[L].get(D)"
+    When referring to an input label, use the formula "self.get_value('input','label',L,D)"
     where L is the label (in strings) and D is the domain element (integer)
 
-    When referring to an input function, use the formula "self.input_to_functions[F].get(D)"
+    When referring to an input function, use the formula "self.get_value('input','function',F,D)"
     where F is the function (in strings) and D is the domain element (integer)
 
-    When referring to a predicate value over the input,  use the formula "self.get_predicate_value(P,D)
+    When referring to a predicate value over the input,  use the formula "self.get_value('input','predicate',P,D)"
     where P is the predicate (in strings) and D is the domain element (integer)"
 
-    When referring to the value of an output node,  use the formula "self.get_output_value(C,L,D)
+    When referring to the label of an output node,  use the formula "self.get_value(C,'label',L,D)"
     where C is the copy (integer), L is the label (in strings), and D is the domain element (integer)"
 
      """
 
     """Example below. Delete"""
-    pred = self.input_to_functions['pred'].get(domain_element)
-    succ = self.input_to_functions['succ'].get(domain_element)
+    pred = self.get_value('input', 'function', 'pred', domain_element)
+    succ = self.get_value('input', 'function', 'succ', domain_element)
 
     if copy is 1 and label == 'stress':
-        if self.get_predicate_value('only',domain_element): return True
-        elif self.get_predicate_value('final',domain_element): return False
-        elif self.input_to_labels['H'].get(domain_element): return True
-        elif self.get_predicate_value('initial',domain_element):return False
-        elif self.get_predicate_value('clash',domain_element): return False
-        else: return self.get_predicate_value('lapse',domain_element)
+        if self.get_value('input', 'predicate', 'only', domain_element):
+            return True
+        elif self.get_value('input', 'predicate', 'final', domain_element):
+            return False
+        elif self.get_value('input', 'label', 'H', domain_element):
+            return True
+        elif self.get_value('input', 'predicate', 'initial', domain_element):
+            return False
+        elif self.get_value('input', 'predicate', 'clash', domain_element):
+            return False
+        else:
+            return self.get_value('input', 'predicate', 'lapse', domain_element)
     if copy == 1 and label == 'H':
-        if self.get_output_value(1, 'stress', domain_element): return True
-        else: return self.input_to_labels['H'].get(domain_element)
+        if self.get_value(1, 'label', 'stress', domain_element):
+            return True
+        else:
+            return self.get_value('input', 'label', 'H', domain_element)
     if copy == 1 and label == 'L':
-        if self.get_output_value(1, 'stress', domain_element): return False
-        else: return self.input_to_labels['L'].get(domain_element)
-
+        if self.get_value(1, 'label', 'stress', domain_element):
+            return False
+        else:
+            return self.get_value('input', 'label', 'L', domain_element)
 
 def personal_Predicate_Formula(self,predicate,domain_element):
     """For every predicate formula, decompose it into the following sequence of if-else clauses:
@@ -90,47 +99,48 @@ def personal_Predicate_Formula(self,predicate,domain_element):
 
     Make sure to use return statements
 
-    When referring to an input label, use the formula "self.input_to_labels[L].get(D)"
+    When referring to an input label, use the formula "self.get_value('input','label',L,D)"
     where L is the label (in strings) and D is the domain element (integer)
 
-    When referring to an input function, use the formula "self.input_to_functions[F].get(D)"
+    When referring to an input function, use the formula "self.get_value('input','function',F,D)"
     where F is the function (in strings) and D is the domain element (integer)
 
-    When referring to a predicate value over the input,  use the formula "self.get_predicate_value(P,D)
+    When referring to a predicate value over the input,  use the formula "self.get_value('input','predicate',P,D)"
     where P is the predicate (in strings) and D is the domain element (integer)"
 
-    When referring to the value of an output node,  use the formula "self.get_output_value(C,L,D)
+    When referring to the label of an output node,  use the formula "self.get_value(C,'label',L,D)"
     where C is the copy (integer), L is the label (in strings), and D is the domain element (integer)"
 
      """
-    pred = self.input_to_functions['pred'].get(domain_element)
-    succ = self.input_to_functions['succ'].get(domain_element)
 
-    """Example below. Delete"""
+
+    pred = self.get_value('input','function','pred',domain_element)
+    succ = self.get_value('input','function','succ',domain_element)
+
     if predicate == 'final':
-        if self.input_to_labels['%'].get(succ): return True
+        if self.get_value('input','label','%',succ): return True
         else: return False
     if predicate == 'initial':
-        if self.input_to_labels['%'].get(pred): return True
+        if self.get_value('input','label','%',pred): return True
         else: return False
     if predicate == 'syll':
-        if self.input_to_labels['L'].get(domain_element): return True
+        if self.get_value('input','label','L',domain_element): return True
         else:
-            return self.input_to_labels['H'].get(domain_element)
+            return self.get_value('input','label','H',domain_element)
     if predicate == 'clash':
-        if self.get_predicate_value('syll', domain_element):
-            return self.get_output_value(1, 'stress', pred)
+        if self.get_value('input','predicate','syll', domain_element):
+            return self.get_value(1,'label', 'stress', pred)
         else:
             return False
     if predicate == 'lapse':
-        if self.get_output_value(1,'stress',pred): return False
+        if self.get_value(1,'label','stress',pred): return False
         else:
-            if self.get_predicate_value('syll',pred): return self.get_predicate_value('syll',domain_element)
+            if self.get_value('input','predicate','syll',pred): return self.get_value('input','predicate','syll',domain_element)
             else: return False
     if predicate == 'only':
-        if self.get_predicate_value('final',domain_element):
-            return self.get_predicate_value('initial',domain_element)
+        if self.get_value('input','predicate','final',domain_element):
+            return self.get_value('input','predicate','initial',domain_element)
         else:
-            if self.get_predicate_value('final',succ):
-                return self.get_predicate_value('initial',domain_element)
+            if self.get_value('input','predicate','final',succ):
+                return self.get_value('input','predicate','initial',domain_element)
             else: return False
