4e75afdfb7ff5652584f1b7713ad147bc8e00057
Hossep Dolatian <hovdeov@gmail.com>
2022-09-26 16:11:32 -0700

    change yaml name

--- bmrs.py
+++ bmrs.py
@@ -1,40 +1,30 @@
 import sys
 
 from logicCompiler import logicCompilation
-from yamlConverter import yamlConversion
+from lispConverter import lispConversion
 
 parameter = sys.argv[1] #
 if parameter == 'convert':
     yamlfile = sys.argv[2]
     if len(sys.argv) >3:
         pythonFileName=sys.argv[3]
-        conversionObj = yamlConversion(yamlfile, pythonFileName)
+        conversionObj = lispConversion(yamlfile, pythonFileName)
     else:
         pythonFileName = yamlfile[:-4] + '.py'
-        conversionObj = yamlConversion(yamlfile, pythonFileName)
+        conversionObj = lispConversion(yamlfile, pythonFileName)
 elif parameter == 'run':
     file =  sys.argv[2]
     word =  sys.argv[3]
     word_type = 'string'
+    # TODO: incorporate freedom to pick word models
     if file[-3:] == '.py':
         runningObj = logicCompilation(word,word_type, file)
         print(f"Output:{runningObj.outputString}")
     else:
         pythonFileName = file[:-4] + '.py'
-        conversionObj = yamlConversion(file, pythonFileName)
+        conversionObj = lispConversion(file, pythonFileName)
         runningObj = logicCompilation(word,word_type, pythonFileName)
         print(f"Output:{runningObj.outputString}")
-
-#
-# outFileArmToLatin = "" + inFile[:-4] + "_ArmToLatin.txt"  ##outfile.txt"##.inFile[:-3] + "txt"
-# outFileLatinToArm = "" + inFile[:-4] + "_LatinToArm.txt"
-#
-# word = 'HLHLH'
-#
-# bmrs = 'Formulas.IterStressHixk'
-#
-# process=Input(word,bmrs)
-
 
 
 
--- lispConverter.py
+++ lispConverter.py
@@ -1,7 +1,7 @@
 import codecs
 import sys
 
-class YamlComponent:
+class LispComponent:
     def __init__(self,name,original_content=[]):
         self.name = name
         self.original_content = original_content
@@ -11,16 +11,16 @@ class YamlComponent:
                f"original_content: {self.original_content}\n" \
                f"info_for_python: {self.info_for_python}"
 
-    def fillInputOutputAlphabet(self,yamlContent):
+    def fillInputOutputAlphabet(self,lispContent):
         assert self.name == "alphabet"
-        assert yamlContent[0] == "def-input-alphabet" or yamlContent[0] == "def-output-alphabet"
-        print(f"Filling up input/output alphabet with content:\n{yamlContent}")
-        assert len(yamlContent) == 2
-        assert type(yamlContent[1]) == list
-        assert yamlContent[1][0] == 'list'
-        self.original_content=yamlContent
+        assert lispContent[0] == "def-input-alphabet" or lispContent[0] == "def-output-alphabet"
+        print(f"Filling up input/output alphabet with content:\n{lispContent}")
+        assert len(lispContent) == 2
+        assert type(lispContent[1]) == list
+        assert lispContent[1][0] == 'list'
+        self.original_content=lispContent
         alphabet_items = set([])
-        for alphabet_item in yamlContent[1][1:]:
+        for alphabet_item in lispContent[1][1:]:
             assert alphabet_item[0] == '"'
             assert alphabet_item[-1] == '"'
             alphabet_items.add(alphabet_item[1:-1])
@@ -32,68 +32,68 @@ class YamlComponent:
             self.info_for_python= alphabet_items
         else:
             self.info_for_python= self.info_for_python.union(alphabet_items)
-    def fillCopySetSize(self,yamlContent):
+    def fillCopySetSize(self,lispContent):
         assert self.name == "copyset"
-        assert yamlContent[0] == "def-copy-set-size"
-        print(f"Filling up copyset size with content:\n{yamlContent}")
-        assert len(yamlContent) == 2
-        assert type(yamlContent[1]) == str
-        self.original_content=yamlContent
-        copysetsize_int = int(yamlContent[1])
+        assert lispContent[0] == "def-copy-set-size"
+        print(f"Filling up copyset size with content:\n{lispContent}")
+        assert len(lispContent) == 2
+        assert type(lispContent[1]) == str
+        self.original_content=lispContent
+        copysetsize_int = int(lispContent[1])
         self.info_for_python= copysetsize_int
 
-class yamlConversion:
-    def __init__(self,originalYaml,pythonFileName):
+class lispConversion:
+    def __init__(self,originalLisp,pythonFileName):
 
         print("Useful debugging info for conversion is printed into the conversion.message.log")
         # The printing code was taken from https://stackoverflow.com/a/2513511
         old_stdout = sys.stdout
-        log_file = open(originalYaml + ".conversion.message.log", "w")
+        log_file = open(originalLisp + ".conversion.message.log", "w")
         sys.stdout = log_file
 
-        self.originalYaml = originalYaml
+        self.originalLisp = originalLisp
         self.pythonFileName = pythonFileName
-          # convert YAML to list
-        self.codeToProcess = self.yamlToList(self.originalYaml)
+          # convert Lisp to list
+        self.codeToProcess = self.lispToList(self.originalLisp)
 
-        #set up obligatory yaml components for the python version
-        self.inputoutput_alphabet_yaml = YamlComponent(name="alphabet")
-        self.copysetsize_yaml = YamlComponent(name="copyset")
-        self.predicateList_yaml = []
-        self.outputFunctList_yaml = []
+        #set up obligatory lisp components for the python version
+        self.inputoutput_alphabet_lisp = LispComponent(name="alphabet")
+        self.copysetsize_lisp = LispComponent(name="copyset")
+        self.predicateList_lisp = []
+        self.outputFunctList_lisp = []
 
-        # assert that the yaml-list has the right structure
+        # assert that the lisp-list has the right structure
         assert len(self.codeToProcess) == 1
         assert self.codeToProcess[0][0] == 'def-transduction'
 
         self.transduction_name = self.codeToProcess[0][1]
         print(f'\nTransuction is for {self.transduction_name}')
 
-        # go through each line in the yaml file and extract the useful information for conversion
-        print("\nWill now read through each line of the Yaml file and extract Yaml components")
-        for yamlComponent in self.codeToProcess[0][2:]:
-            print(f'\tReading component:\n{yamlComponent}')
-            yamlComponentName = yamlComponent[0]
-            if yamlComponentName ==  'def-input-alphabet':
-                self.inputoutput_alphabet_yaml.fillInputOutputAlphabet(yamlComponent)
-                print(f'\t->Updated input/output alphabet:\n{self.inputoutput_alphabet_yaml}')
-            elif yamlComponentName == 'def-output-alphabet':
-                self.inputoutput_alphabet_yaml.fillInputOutputAlphabet(yamlComponent)
-                print(f'\t->Updated input/output alphabet:\n{self.inputoutput_alphabet_yaml}')
-            elif yamlComponentName == 'def-copy-set-size':
-                self.copysetsize_yaml.fillCopySetSize(yamlComponent)
-                print(f'\t->Updated copyset size:\n{self.copysetsize_yaml}')
-            elif yamlComponentName == 'def-predicate':
-                self.insertPredicateYamlToList(yamlComponent)
-                print(f'->Updated predicate list:\n{self.predicateList_yaml}')
-            elif yamlComponentName == 'def-output':
-                self.insertOutputFunctYamlToList(yamlComponent)
-                print(f'->Updated output label function list:\n{self.outputFunctList_yaml}')
+        # go through each line in the lisp file and extract the useful information for conversion
+        print("\nWill now read through each line of the Lisp file and extract Lisp components")
+        for lispComponent in self.codeToProcess[0][2:]:
+            print(f'\tReading component:\n{lispComponent}')
+            lispComponentName = lispComponent[0]
+            if lispComponentName ==  'def-input-alphabet':
+                self.inputoutput_alphabet_lisp.fillInputOutputAlphabet(lispComponent)
+                print(f'\t->Updated input/output alphabet:\n{self.inputoutput_alphabet_lisp}')
+            elif lispComponentName == 'def-output-alphabet':
+                self.inputoutput_alphabet_lisp.fillInputOutputAlphabet(lispComponent)
+                print(f'\t->Updated input/output alphabet:\n{self.inputoutput_alphabet_lisp}')
+            elif lispComponentName == 'def-copy-set-size':
+                self.copysetsize_lisp.fillCopySetSize(lispComponent)
+                print(f'\t->Updated copyset size:\n{self.copysetsize_lisp}')
+            elif lispComponentName == 'def-predicate':
+                self.insertPredicateLispToList(lispComponent)
+                print(f'->Updated predicate list:\n{self.predicateList_lisp}')
+            elif lispComponentName == 'def-output':
+                self.insertOutputFunctLispToList(lispComponent)
+                print(f'->Updated output label function list:\n{self.outputFunctList_lisp}')
             else:
-                print(f'This Yaml component could not be processed:{yamlComponent}')
+                print(f'This Lisp component could not be processed:{lispComponent}')
                 exit()
 
-        print("\nFinished reading the Yaml file; will now convert to Python")
+        print("\nFinished reading the Lisp file; will now convert to Python")
         print('\n1) Creating the input alphabet (input symbols)')
         self.processInputAlphabet()
         print('\n2) Creating feature set')
@@ -155,12 +155,12 @@ class yamlConversion:
         else:
             return groups
 
-    def yamlToList(self,originalYaml):
-        # Given a YAML file, convert it into a list
-        print("Creating a cleaned up YAML file")
-        outFileCleaned = originalYaml[:-4] + "_cleaned.txt"
+    def lispToList(self,originalLisp):
+        # Given a Lisp file, convert it into a list
+        print("Creating a cleaned up Lisp file")
+        outFileCleaned = originalLisp[:-4] + "_cleaned.txt"
         codeToProcess = ""
-        with codecs.open(originalYaml, 'r', 'utf-8') as iFile:
+        with codecs.open(originalLisp, 'r', 'utf-8') as iFile:
             with codecs.open(outFileCleaned, 'w', 'utf-8') as o:
                 lines = iFile.read().splitlines()
                 lines[0] = lines[0].replace('\ufeff', "")
@@ -175,33 +175,33 @@ class yamlConversion:
                         codeToProcess = codeToProcess + headStripped + ' '
         print(f'Reading the following code:\n{(codeToProcess)}')
         codeToProcess = self.parse_parentheses(codeToProcess)
-        print("->YAML file is parsed to the following list")
+        print("->Lisp file is parsed to the following list")
         print(codeToProcess)
         print("->A more readable version of the list:")
         for i in range(len(codeToProcess[0])):
             print(codeToProcess[0][i])
         return codeToProcess
-    def insertPredicateYamlToList(self,yamlContent):
-        assert yamlContent[0] == "def-predicate"
-        print(f"Adding a predicate-yaml to the predicate list"
-              f"\n{yamlContent}")
-        assert len(yamlContent) == 4
-        self.original_content=yamlContent
-        predicate_name = yamlContent[1]
-        predicate_node = yamlContent[2]
-        predicate_logic = yamlContent[3]
-        self.predicateList_yaml.append({'name':predicate_name,'node':predicate_node,'logic':predicate_logic})
-    def insertOutputFunctYamlToList(self,yamlContent):
-        assert yamlContent[0] == "def-output"
-        print(f"Adding a output-yaml to the output-function list"
-              f"\n{yamlContent}")
-        assert len(yamlContent) == 5
-        self.original_content=yamlContent
-        output_name = yamlContent[1]
-        output_copy = yamlContent[2]
-        output_node= yamlContent[3]
-        output_logic = yamlContent[4]
-        self.outputFunctList_yaml.append({'name':output_name,'copy':output_copy,'node':output_node,'logic':output_logic})
+    def insertPredicateLispToList(self,lispContent):
+        assert lispContent[0] == "def-predicate"
+        print(f"Adding a predicate-lisp to the predicate list"
+              f"\n{lispContent}")
+        assert len(lispContent) == 4
+        self.original_content=lispContent
+        predicate_name = lispContent[1]
+        predicate_node = lispContent[2]
+        predicate_logic = lispContent[3]
+        self.predicateList_lisp.append({'name':predicate_name,'node':predicate_node,'logic':predicate_logic})
+    def insertOutputFunctLispToList(self,lispContent):
+        assert lispContent[0] == "def-output"
+        print(f"Adding a output-lisp to the output-function list"
+              f"\n{lispContent}")
+        assert len(lispContent) == 5
+        self.original_content=lispContent
+        output_name = lispContent[1]
+        output_copy = lispContent[2]
+        output_node= lispContent[3]
+        output_logic = lispContent[4]
+        self.outputFunctList_lisp.append({'name':output_name,'copy':output_copy,'node':output_node,'logic':output_logic})
     def processLabelFeatureSet(self):
         #TODO: rewrite this after eric figures out features
         self.inputLabelFeaturesPython =  """\tself.labels_list = [ """
@@ -213,7 +213,7 @@ class yamlConversion:
         print(f'->Finished creating python code for label/features:\n{self.inputLabelFeaturesPython}')
     def processInputAlphabet(self):
         self.inputoutput_alphabetPython = """\tself.input_symbols = [ """
-        self.inputoutput_alphabetList = list(self.inputoutput_alphabet_yaml.info_for_python)
+        self.inputoutput_alphabetList = list(self.inputoutput_alphabet_lisp.info_for_python)
         for symbol in (self.inputoutput_alphabetList)[:-1]:
             print(f'Working on:{symbol}')
             self.inputoutput_alphabetPython = self.inputoutput_alphabetPython + f"'{symbol}', "
@@ -223,22 +223,22 @@ class yamlConversion:
         self.inputoutput_alphabetList = self.inputoutput_alphabetList + ['%','#']
         print(f'Had to include #,% in our list of symbols, but not in the python code:\n{self.inputoutput_alphabetList}')
     def processCopySet(self):#
-        self.copySetMaxValue = self.copysetsize_yaml.info_for_python
+        self.copySetMaxValue = self.copysetsize_lisp.info_for_python
         self.copySetPython = f'\tself.copyset = {list(range(self.copySetMaxValue+1))[1:]}'
         print(f'->Finished creating python code for copyset:\n{self.copySetPython}')
 
 
     def processOutputLabelFormulaList(self):
         self.outputLabelFormulaPythonList = []
-        for outputLabelFunction_listifid_yaml in self.outputFunctList_yaml:
-            print(f'Working on output label function code from yaml:\n{outputLabelFunction_listifid_yaml}')
-            outputLabelFormula_label = outputLabelFunction_listifid_yaml['name']
-            outputLabelFormula_copy = outputLabelFunction_listifid_yaml['copy']
-            outputLabelFormula_node = outputLabelFunction_listifid_yaml['node']
+        for outputLabelFunction_listifid_lisp in self.outputFunctList_lisp:
+            print(f'Working on output label function code from lisp:\n{outputLabelFunction_listifid_lisp}')
+            outputLabelFormula_label = outputLabelFunction_listifid_lisp['name']
+            outputLabelFormula_copy = outputLabelFunction_listifid_lisp['copy']
+            outputLabelFormula_node = outputLabelFunction_listifid_lisp['node']
             assert outputLabelFormula_node == ['x']
-            outputLabelFormula_logicYaml = outputLabelFunction_listifid_yaml['logic']
+            outputLabelFormula_logicLisp = outputLabelFunction_listifid_lisp['logic']
             outputLabelFormula_python_ListItem =f'\tif copy is {outputLabelFormula_copy} and label == {outputLabelFormula_label}:'
-            logicBody = self.processLogicBody(outputLabelFormula_logicYaml,"\t\t")
+            logicBody = self.processLogicBody(outputLabelFormula_logicLisp,"\t\t")
             outputLabelFormula_python_ListItem = outputLabelFormula_python_ListItem + f"\n{logicBody}"
             print(f'->Created:\n{outputLabelFormula_python_ListItem}')
             outputLabelFormula_python_ListItem = self.addReturnsToLogicBody(outputLabelFormula_python_ListItem)
@@ -252,9 +252,9 @@ class yamlConversion:
         self.predicateListNames = []
         self.predicateListNamePython = ""
         self.predicateLogicPython = []
-        for predicateYaml in self.predicateList_yaml:
-            print(f'Reading predicate-yaml: {predicateYaml}')
-            self.predicateListNames.append(predicateYaml['name'])
+        for predicateLisp in self.predicateList_lisp:
+            print(f'Reading predicate-lisp: {predicateLisp}')
+            self.predicateListNames.append(predicateLisp['name'])
         if len(self.predicateListNames) is 0:
             print("There are no predicates to creates")
         else:
@@ -268,10 +268,10 @@ class yamlConversion:
 
             print(f'->Creating the following list of predicates: {self.predicateListNames}')
 
-            for predicateYaml in self.predicateList_yaml:
-                predicateText = f"\tif level is 0 and predicate == '{predicateYaml['name']}':"
-                assert predicateYaml['node'] == ['x']
-                logicBody=self.processLogicBody(predicateYaml['logic'],"\t\t")
+            for predicateLisp in self.predicateList_lisp:
+                predicateText = f"\tif level is 0 and predicate == '{predicateLisp['name']}':"
+                assert predicateLisp['node'] == ['x']
+                logicBody=self.processLogicBody(predicateLisp['logic'],"\t\t")
                 predicateText = predicateText + f"\n{logicBody}"
                 print(f'Created:\n{predicateText}')
                 predicateText=self.addReturnsToLogicBody(predicateText)
@@ -285,7 +285,7 @@ class yamlConversion:
         elif text == "'false":
                 return f'{tabs}False'
         elif len(text) is 2:
-            # Checks if the yaml codeblock is like (#_0 x) or (predicate x)')
+            # Checks if the lisp codeblock is like (#_0 x) or (predicate x)')
             labelOrPred,copy,node= None, None, None
             firstElement =  text[0]
             secondElement = text[1]
@@ -299,15 +299,15 @@ class yamlConversion:
             # Right now, the nodeFinding method is used to retrieve a node (copy,domain_element)
             # But if an output-label function is calling this, then we just need the domain element which we have to extract
             # Once we incorpoate output-function formula, then this will need to be revised
-            # TODO: what we need is to incorporate indexing better in the YAML
+            # TODO: what we need is to incorporate indexing better in the Lisp
             logicbody = f"{tabs}self.get_BooleanValue('{islabelOrPred}','{labelOrPred}',({copy},self.get_NodeDomain({node})))"
             print(f'->Processed this body and returned:{logicbody}')
             return logicbody
         elif len(text) is 4:
-            # Checks if the yaml codebloc is like (if CONDITION RESULT OTHERWISE
-            firstIf,secondCondYaml,thirdRes,fourthElse = text
+            # Checks if the lisp codebloc is like (if CONDITION RESULT OTHERWISE
+            firstIf,secondCondLisp,thirdRes,fourthElse = text
             assert firstIf == 'if'
-            secondCondLogic = self.processLogicBody(secondCondYaml,'')
+            secondCondLogic = self.processLogicBody(secondCondLisp,'')
             logicbody = f'{tabs}if {secondCondLogic}:\n'
             thirdResultLogic = self.processLogicBody(thirdRes,f'{tabs}\t')
             logicbody = f"{logicbody}{thirdResultLogic}"
@@ -378,7 +378,7 @@ class yamlConversion:
 
     def createPythonFile(self):
         with codecs.open(self.pythonFileName, 'w', 'utf-8') as o:
-            o.writelines(f"#Automatically generated Python file from YAML:{self.transduction_name}\n")
+            o.writelines(f"#Automatically generated Python file from Lisp:{self.transduction_name}\n")
             o.writelines(f"def personal_setup(self):\n")
             o.writelines(f"{self.inputoutput_alphabetPython}\n")
             o.writelines(f"{self.inputLabelFeaturesPython}\n")
